a9c47bde4aebbca5a8dec8647404d1ad57ddf631
Olaboji <olasojimakinwa@gmail.com>
2018-01-03 15:20:45 +0100

    3 rondes toegevoegd
    zie titel

--- xd/ZenuwSpiraal/ZenuwSpiraal.ino
+++ xd/ZenuwSpiraal/ZenuwSpiraal.ino
@@ -11,7 +11,7 @@ const int high = 0;
 static bool gstart = false;
 static bool gdraad = false;
 static bool geind = false;
-
+static bool ground = false;
 //leds
 const int LSTART = 10;
 const int LDRAAD = 11;
@@ -25,6 +25,9 @@ int eind = A3;
 static unsigned long tijd = 0;
 unsigned long refTime = 0 ;
 float ftijd = 0;
+
+int rounds = 3;
+static float highscore = 100.00;
 void setup() {
   Timer1.initialize();
   MFS.initialize(&Timer1);
@@ -49,56 +52,67 @@ void ledOnOF(bool geheugen, int led) {
   }
 }
 
+
 void loop() {
-  if (digitalRead(startt) == high) {
-    gstart = true;
-    gdraad  = false;
-    geind = false;
-  }
-  if (digitalRead(draad) == high || digitalRead(eind) == high) {
-    gstart = false;
-    if (digitalRead(draad) == high) {
-      if (geind == true) {
-        gdraad = false;
-      } else {
-        gdraad = true;
+  while (rounds > 0) {
+    if (digitalRead(startt) == high) {
+      gstart = true;
+      gdraad  = false;
+      geind = false;
+    }
+    if (digitalRead(draad) == high || digitalRead(eind) == high) {
+      gstart = false;
+      ground = true;
+      if (digitalRead(draad) == high) {
+        if (geind == true) {
+          gdraad = false;
+        } else {
+          gdraad = true;
+        }
+      }
+      if (digitalRead(eind) == high) {
+        if (gdraad == true) {
+          geind = false;
+        } else {
+          geind = true;
+        }
       }
     }
-    if (digitalRead(eind) == high) {
-      if (gdraad == true) {
-        geind = false;
-      } else {
-        geind = true;
-      }
+    if (digitalRead(startt) == high) {
+      tijd = millis();
+      refTime = millis();
+      ftijd = 0.00;
     }
-  }
-  if (digitalRead(startt) == high) {
-    tijd = millis();
-    refTime = millis();
-    ftijd=0.00;
-  }
 
 
-  if (gdraad == true || geind == true || ftijd == 99.99) {
-    //pause
-  } else {
-    tijd = millis();
-    ftijd = (tijd - refTime) / 1000.00;
-    MFS.write(ftijd, 2);
-  }
-  //pieper
-  if (geind == false) {
-    if (digitalRead(draad) == high) {
-      digitalWrite(3, high);
+    if (gdraad == true || geind == true || ftijd == 99.99) {
+      //pause
     } else {
-      digitalWrite(3, low);
+      tijd = millis();
+      ftijd = (tijd - refTime) / 1000.00;
+      MFS.write(ftijd, 2);
     }
+    //pieper
+    if (geind == false) {
+      if (digitalRead(draad) == high) {
+        digitalWrite(3, high);
+      } else {
+        digitalWrite(3, low);
+      }
+    }
+    if (ground == true && rounds == 1) {
+      rounds == 3;
+      break;
+    } else if (ground == true && gstart == true){
+      rounds--;
+      ground = false;
+    }
+
+    ledOnOF(gstart, LSTART);
+    ledOnOF(gdraad, LDRAAD);
+    ledOnOF(geind, LEIND);
+
   }
-
-  ledOnOF(gstart, LSTART);
-  ledOnOF(gdraad, LDRAAD);
-  ledOnOF(geind, LEIND);
-
-
-
+  MFS.write("fini");
+  delay(1000);
 }
